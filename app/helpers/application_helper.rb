9bb21d4a7d6c5933e3bff1085d9d61c152cd9d06
George <skywalqqer@yandex.ru>
2016-12-11 18:14:15 +0300

    add article CRUD and write style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  class HTMLWithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
+  def markdown(text)
+    renderer = HTMLWithPygments.new(hard_wrap: true, filter_html: true)
+    options = {
+        autolink: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        lax_html_blocks: true,
+        strikethrough: true,
+        superscript: true,
+        space_after_headers: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
