cf10a6c4f9e55e06f2a21259de3fcbeff47732ca
George <skywalqqer@yandex.ru>
2016-12-11 20:59:08 +0300

    set up send email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'KingCode',
+    :password => '89273223910q',
+    :domain => 'https://kingcode.herokuapp.com/',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
