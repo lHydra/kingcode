49941ee0e4888cf4989a82e0a376048c8f65246e
George Melnikov <skywalqqer@yandex.ru>
2016-12-14 23:14:02 +0500

    add projects (#4)
    * add CRUD projects and style for this
    
    * delete style variable from import files and add some rules

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @articles = Article.last(3)
+    @projects = Project.last(3)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211155836) do
+ActiveRecord::Schema.define(version: 20161212204307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20161211155836) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
