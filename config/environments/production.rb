0dc194de45e4dc9ad12635aa0c04541807a4c607
george <skywalqqer@yandex.ru>
2018-03-24 02:22:33 +0300

    set up deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://kingcode.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://kingcode.ru/' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'kingcode.ru',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
